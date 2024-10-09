34829332eea3457be9eba649371b3752ccecc6f4
nadiaparsell <parsell@usc.edu>
2024-10-09 13:36:27 +0900

    Implement DataQualityOperator to check table row counts with logging and failure handling for Redshift

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -2,21 +2,32 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
+
 class DataQualityOperator(BaseOperator):
 
     ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 tables="",
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        for table in self.tables:
+            self.log.info(f"Checking data quality for table {table}")
+            records = redshift.get_records(f"SELECT COUNT(*) FROM {table}")
+            if len(records) < 1 or len(records[0]) < 1:
+                raise ValueError(
+                    f"DATA QUALITY CHECK FAILED. {table} has no records")
+            num_records = records[0][0]
+            if num_records < 1:
+                raise ValueError(
+                    f"DATA QUALITY CHECK FAILED. {table} HAS 0 records")
+            self.log.info(
+                f"DATA QUALITY CHECK PASSED. {table} has {records[0][0]} records")
