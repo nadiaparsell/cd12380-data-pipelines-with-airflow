c81739dfddaf5e02424eefab24c0afe213252f79
nadiaparsell <parsell@usc.edu>
2024-10-09 12:48:28 +0900

    Implement DAG to include dimension tables

--- dags/final_project.py
+++ dags/final_project.py
@@ -59,18 +59,34 @@ def final_project():
 
     load_user_dimension_table = LoadDimensionOperator(
         task_id='Load_user_dim_table',
+        redshift_conn_id='redshift',
+        table='users',
+        sql_query=SqlQueries.user_table_insert,
+        mode='truncate-insert'
     )
 
     load_song_dimension_table = LoadDimensionOperator(
         task_id='Load_song_dim_table',
+        redshift_conn_id='redshift',
+        table='songs',
+        sql_query=SqlQueries.song_table_insert,
+        mode='truncate-insert'
     )
 
     load_artist_dimension_table = LoadDimensionOperator(
         task_id='Load_artist_dim_table',
+        redshift_conn_id='redshift',
+        table='artists',
+        sql_query=SqlQueries.artist_table_insert,
+        mode='truncate-insert'
     )
 
     load_time_dimension_table = LoadDimensionOperator(
         task_id='Load_time_dim_table',
+        redshift_conn_id='redshift',
+        table='time',
+        sql_query=SqlQueries.time_table_insert,
+        mode='truncate-insert'
     )
 
     run_quality_checks = DataQualityOperator(
