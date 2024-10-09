d45742a5f02f5e7fc2af16f45e6f34b53b326065
nadiaparsell <parsell@usc.edu>
2024-10-09 13:37:44 +0900

    Implement DAG with data quality.

--- dags/final_project.py
+++ dags/final_project.py
@@ -91,6 +91,8 @@ def final_project():
 
     run_quality_checks = DataQualityOperator(
         task_id='Run_data_quality_checks',
+        redshift_conn_id='redshift',
+        tables=['dev.public.users', 'songplays', 'songs', 'artists', 'time']
     )
 
     end_operator = DummyOperator(task_id='Stop_execution')
